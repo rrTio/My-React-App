4ceb1674d4ed094670a6fd1437093e8067783cd3
rrTio <tio.rozsraven@ue.edu.ph>
2023-03-25 22:34:23 +0800

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [count, setCount] = useState(0);
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button className='button buttonAdd' >ADD</button>
+      <br/>
+      {count}
+      <br/>
+      <button className='button buttonSub'>SUBTRACT</button>
     </div>
   );
 }
