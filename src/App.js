00ad5fc242903c29eb3faffb57f9963d0bb4132a
rrTio <tio.rozsraven@ue.edu.ph>
2023-04-07 01:01:16 +0800

    Added add and subtract function

--- src/App.js
+++ src/App.js
@@ -3,19 +3,27 @@ import ReactDOM from 'react-dom';
 import { useState } from 'react';
 import './App.css';
 
-function App() {
+export default function App() {
 
   const [count, setCount] = useState(0);
   
+  function btnAdd(){
+    setCount(count + 1);
+  }
+
+  function btnSub(){
+    setCount(count - 1);
+  }
+
   return (
     <div className="App">
-      <button className='button buttonAdd' >ADD</button>
+      <button className='button buttonAdd' onClick={btnAdd}>ADD</button>
       <br/>
       {count}
       <br/>
-      <button className='button buttonSub'>SUBTRACT</button>
+      <button className='button buttonSub' onClick={btnSub}>SUBTRACT</button>
     </div>
   );
 }
 
-export default App;
+
