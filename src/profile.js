a4e7177ecee1ce64990ab5a0a79a67f0560b93c9
rrTio <tio.rozsraven@ue.edu.ph>
2023-04-08 01:15:15 +0800

    Added forms in profile.js

--- src/profile.js
+++ src/profile.js
@@ -1,17 +1,44 @@
 import React from 'react';
 import './profile.css'
+import { useState } from 'react';
 
 export default function MyProfile(){
 
-    const user = {
-        firstName: 'Rozs Raven',
-        lastName: 'Tio'
+    const [inputs, setInputs] = useState({});
+
+    const handleChange = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        setInputs(values => ({...values, [name]: value}))
+    }
+    
+    const submission = (event) => {
+        event.preventDefault();
+        console.log(inputs)
     }
     return(
+        <div>
+            <center>
+                <h1>REGISTRATION FORM</h1>
+                <form onSubmit={submission}>
+                    <label>LAST NAME: </label>
+                    <input type="text" name='lastName' value={inputs.lastName || ""} onChange={handleChange}/>
 
-        <>
-            <h1>Name: {user.firstName + " " + user.lastName}</h1>
-        </>
-        
+                    <br/>
+
+                    <label>FIRST NAME: </label>
+                    <input type="text" name='firstName' value={inputs.firstName || ""} onChange={handleChange}/>
+
+                    <br/>
+
+                    <label>MIDDLE NAME: </label>
+                    <input type="text" name='middleName' value={inputs.middleName || ""} onChange={handleChange}/>
+
+                    <br/>
+
+                    <button>REGISTER</button>
+                </form>
+            </center>
+        </div>
     );
 }
